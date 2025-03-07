38c575714db071f6c78eea516edfb01f7a177bb2
David Rischow <drischow@stud.hs-heilbronn.de>
2025-03-07 22:37:02 +0100

    change production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,20 +97,16 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.assets.compile = true
   config.assets.digest = true
-  config.action_mailer.raise_delivery_errors = true
-config.action_mailer.perform_caching = false
-
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "https://letsgovolunteer-540e129e3acc.herokuapp.com",
-  user_name: ENV["SMTP_USERNAME"],  
-  password: ENV["SMTP_PASSWORD"],  
-  authentication: "plain",
-  enable_starttls_auto: true
-}
-config.action_mailer.default_url_options = { host: "volunteer-heilbronn.de" }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "letsgovolunteer.com",
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
 
 end
