3900dd2a76b5078f3b75e35ad928dbe2b22a3c52
David Rischow <drischow@stud.hs-heilbronn.de>
2025-03-26 23:48:23 +0100

    edit safelist (again)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,10 +4,11 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.enable_reloading = false
+  config.enable_reloading = true
   config.assets.compile = true # This is important for Heroku
   config.assets.css_compressor = nil
-  
+  config.assets.digest = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-'./app/views/**/*.{html,html.erb,erb}',
+    './app/views/**/*.{html,html.erb,erb}',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/assets/stylesheets/**/*.css',
