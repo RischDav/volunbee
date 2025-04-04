2685d78ca024173d1ea9cab6f5d4e464d484a88e
David Rischow <drischow@stud.hs-heilbronn.de>
2025-04-04 13:03:32 +0200

    some redesigns

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,30 @@
 Rails.application.routes.draw do
-  get "static_pages/privacy"
-  get "static_pages/imprint"
-
   # Locale-spezifische Routen
   scope "(:locale)", locale: /en|de/ do
-    root to: "positions#index" # Definiert die Startseite mit optionaler Locale
-    resources :positions
-  end
-  
-  devise_for :users, controllers: {
-    registrations: "users/registrations",
-    confirmations: "users/confirmations",
-    sessions: 'users/sessions'
-  }
+    # Startseite
+    root to: "positions#index"
 
-  resources :organizations do
-    member do
-      patch 'release'
-      patch 'lock'
+    # Statische Seiten
+    get "static_pages/privacy"
+    get "static_pages/imprint"
+
+    # Benutzerbezogene Routen
+    get 'users/locked', to: 'users#locked', as: 'users_locked'
+
+    # Ressourcen
+    resources :positions
+    resources :organizations do
+      member do
+        patch 'release'
+        patch 'lock'
+      end
     end
+
+    # Devise-Routen
+    devise_for :users, controllers: {
+      registrations: "users/registrations",
+      confirmations: "users/confirmations",
+      sessions: 'users/sessions'
+    }
   end
 end
