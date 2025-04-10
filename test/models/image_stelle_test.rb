d34311e17d5336f8532748337e87e6390c12734d
damino-code <med.amine.jradi123@gmail.com>
2025-04-10 16:31:44 +0200

    complete translation of the shared folder, still not done with testing

--- test/models/image_stelle_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ImageStelleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Use fixtures to load data
+  fixtures :users, :organizations
+
+  def setup
+    @user = users(:regular_user)    # Use the user fixture data
+    @admin = users(:admin)  # Use the admin fixture data
+   # @organization = organizations(:org_one)  # Use the organization fixture data
+  end
+
+  # Test that user is valid with correct attributes
+  test "should be valid with valid attributes" do
+    assert @user.valid?
+    assert @admin.valid?
+  end
+
 end
