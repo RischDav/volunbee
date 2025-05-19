b37ce99bb41a26175474c7d7c3d8ac5347666c0e
damino-code <med.amine.jradi123@gmail.com>
2025-05-19 14:49:56 +0200

    content update

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -1,5 +1,6 @@
 class MatchingController < ApplicationController
     skip_before_action :authenticate_user!, only: [:index]
+    layout 'volunteer'
     def index
     end
   end
--- app/controllers/show_positions_controller.rb
+++ app/controllers/show_positions_controller.rb
@@ -1,5 +1,6 @@
 class ShowPositionsController < ApplicationController
     skip_before_action :authenticate_user!, only: [:index]
+    layout 'volunteer'
     def index
     end
   end
