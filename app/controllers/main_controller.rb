713f23df2c6347055ee8a1addf44db0a117e5fe5
David Rischow <drischow@stud.hs-heilbronn.de>
2025-06-15 21:17:11 +0200

    show only positions on homepage which are released

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,3 @@
-class MainController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-  layout 'volunteer'
-  
-  def index
-    @positions = Position.all.select { |position| position.main_picture.attached? }.take(3)
-    
-  end
+def index
+  @positions = Position.where(released: true, online: true).select { |position| position.main_picture.attached? }.take(3)
 end
