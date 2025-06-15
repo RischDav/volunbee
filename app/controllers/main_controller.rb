aad1e4947eeb1e4178275e0fae230f8bd7380159
David Rischow <drischow@stud.hs-heilbronn.de>
2025-06-15 21:23:50 +0200

    fix main controller error

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,5 @@
-def index
-  @positions = Position.where(released: true, online: true).select { |position| position.main_picture.attached? }.take(3)
+class MainController < ApplicationController
+  def index
+    @positions = Position.where(released: true, online: true).select { |position| position.main_picture.attached? }.take(3)
+  end
 end
