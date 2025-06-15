02ff6def022835777e2aedeaa12f6cdc771d9f77
David Rischow <drischow@stud.hs-heilbronn.de>
2025-06-15 21:29:36 +0200

    access main page without login

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,5 @@
 class MainController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
     @positions = Position.where(released: true, online: true).select { |position| position.main_picture.attached? }.take(3)
   end
