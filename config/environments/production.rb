8b881b4804865af85faad0a9aaca0892ad1f2433
David Rischow <drischow@stud.hs-heilbronn.de>
2025-06-16 16:21:31 +0200

    resolve cookie bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Mailer
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "dashboard.volunteer-heilbronn.de", protocol: 'https' }
+  config.action_mailer.default_url_options = { host: "volunbee.com", protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
