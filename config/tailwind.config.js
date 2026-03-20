870825101e0629d5c6905f39e7abf73eeb103342
David Rischow <drischow@stud.hs-heilbronn.de>
2026-03-20 22:01:15 +0100

    deactivate darkmode

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 
 module.exports = {
+  darkMode: 'class',
   content: [
       './app/views/**/*.{erb,html, html.erb}',
       './app/helpers/**/*.rb',
